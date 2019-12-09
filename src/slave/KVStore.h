06557d2012c828ed3a40710d48dbcf8b34a5e692
PasinduMuthukuda <pasindumuth@gmail.com>
2019-12-08 19:45:45 -0800

    Better documentation of KVStore

--- src/slave/KVStore.h
+++ src/slave/KVStore.h
@@ -15,6 +15,31 @@
 namespace uni {
 namespace slave {
 
+/**
+ * @brief A representation of a multi-version key-value store (MVKVS).
+ * 
+ * Each key, a string value, maps to a list of (value, timestamp) pairs. These pairs are
+ * called versions. Version lists are modified as append only, with each subsequent version
+ * has a greater timestamp. This MVKVS enables snapshot reads; if a key exists, a read of the
+ * kay at a certain time will always return the same value.
+ * 
+ * To read from a MVKVS, a client passes in a key and timestamp. The version with timestamp
+ * before or at the provided timestamp is returned. If there is no such key, or no such version,
+ * then an empty optional is returned.
+ * 
+ * To write to a MVKVS, a client passes in a key, value, and timestamp. The timestamp must be
+ * strictly greater than the last timestamp that the key was accessed. This includes after a read.
+ * So for example, if the last timestamp for a key happened from a write at time 10, and then a
+ * read happened for that key at timestamp 13, then a subsequent write cannot write to the key at
+ * any time <= 13. If this was possible, a subsequent read at timestamp 13 would return a different
+ * value, defeating the ability to do snapshot reads.
+ * 
+ * Note that last access times apply to keys, not the whole MVKVS. That is, a key k1 can have 
+ * last access time 10, and another key k2 can have last access time 5. Thus, a write to k2
+ * can happen at time 8 without any worry.
+ * 
+ * All data is stored in memory.
+ */ 
 class KVStore {
  public:
   struct MultiVersionEntry {
@@ -27,12 +52,36 @@ class KVStore {
     timestamp_t lat; // Last access timestamp (write or read)
   };
   
+  /**
+   * @brief Write a new version to the given key
+   * 
+   * If the key exists, then the timestamp must be greater than the last access time
+   * of the key.
+   */ 
   void write(std::string key, std::string value, timestamp_t timestamp);
 
+  /**
+   * @brief Read the value of a key at the given timestamp
+   * 
+   * If the key doesn't exist, returns an empty optiona. If it does exist, then the version whose
+   * timestamp is <= than the @p timestamp is retried, and it's value returned. If no such
+   * version exists (the @p timestamp is earlier than the earlier version), then an empty optiona
+   * is returned.
+   */
   boost::optional<std::string> read(std::string key, timestamp_t timestamp);
 
+  /**
+   * @brief Get a callback that can be registered with a uni::paxos::PaxosLog
+   * 
+   * This callback is used in the uni::paxos::PaxosLog to update the KVStore when
+   * a proto::paxos::PaxosLogEntry is entered into the uni::paxos::PaxosLog. This includes
+   * both writes as well as reads (which updates the last access time).
+   */
   std::function<void(uni::paxos::index_t, proto::paxos::PaxosLogEntry)> get_paxos_callback();
 
+  /**
+   * @brief generated a pretty printed debug string (JSON-like indentation).
+   */
   std::string debug_string() const;
 
  private:
--- testing/unit/slave/KVStoreTest.cc
+++ testing/unit/slave/KVStoreTest.cc
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 
+#include <assert/UniversalException.h>
 #include <slave/KVStore.h>
 
 namespace uni {
@@ -13,20 +14,47 @@ class KVStoreTest
    void SetUp() override {
     _kvstore.write("k", "v1", 0);
     _kvstore.write("k", "v2", 2);
-    _kvstore.write("k", "v4", 4);
-    _kvstore.write("k1", "v1", 3);
+    _kvstore.write("k", "v3", 4);
+    _kvstore.write("k1", "v4", 3);
    }
 
    uni::slave::KVStore _kvstore;
 };
 
+/**
+ * Ensures that the values read for different keys at different timestamps
+ * are correct.
+ */ 
 TEST_F(KVStoreTest, ReadTest) {
   EXPECT_EQ(_kvstore.read("k", 0), boost::none) << "Value read should be none.";
   EXPECT_EQ(_kvstore.read("k", 1).get(), "v1") << "Wrong reading value.";
   EXPECT_EQ(_kvstore.read("k", 3).get(), "v2") << "Wrong reading value.";
-  EXPECT_EQ(_kvstore.read("k", 5).get(), "v4") << "Wrong reading value.";
-  EXPECT_EQ(_kvstore.read("k1", 4).get(), "v1") << "Wrong reading value.";
+  EXPECT_EQ(_kvstore.read("k", 5).get(), "v3") << "Wrong reading value.";
+  EXPECT_EQ(_kvstore.read("k1", 4).get(), "v4") << "Wrong reading value.";
 }
+
+/**
+ * Ensures that a value cannot be written to a key with a timestamp before a previous
+ * read or write of the key.
+ */ 
+TEST_F(KVStoreTest, WriteToThePastAfterRead) {
+  // When reading a value at a given timestamp
+  _kvstore.read("k1", 5).get();
+  // And when trying to write a value to an earlier timestamp for the same key,
+  // Then expect an exception to be thrown.
+  ASSERT_THROW(_kvstore.write("k1", "v5", 4), uni::assert::UniversalException);
+}
+
+// /**
+//  * Ensure that value can be written for the same key at different timestamps
+//  * and the read values are as expected.
+//  */
+// TEST_F(KVStoreTest, WriteTest) {
+//   EXPECT_EQ(_kvstore.read("k1", 5).get(), "v4") << "Wrong reading value.";
+//   _kvstore.write("k1", "v5", 6);
+//   EXPECT_EQ(_kvstore.read("k1", 5).get(), "v4") << "Wrong reading value.";
+//   EXPECT_EQ(_kvstore.read("k1", 6).get(), "v5") << "Wrong reading value.";
+// }
 
 } // namespace slave
 } // namespace unit
