7b4de4e0801e9423e5cf1cfc65d3e7e79b35c1ee
PasinduMuthukuda <pasindumuth@gmail.com>
2020-03-23 18:23:50 -0700

    Upgraded tests client to take in CLI-like commands with arguments, and added a command for finding key spaces and inserting values

--- src/testclient.cc
+++ src/testclient.cc
@@ -19,7 +19,7 @@ using boost::asio::ip::tcp;
 
 std::vector<std::string> parse_input(std::string&& message) {
   auto parsed_output = std::vector<std::string>();
-  auto delimiter = std::string(", ");
+  auto delimiter = std::string(" ");
   auto pos = 0;
   while ((pos = message.find(delimiter)) != std::string::npos) {
       auto token = message.substr(0, pos);
@@ -30,54 +30,89 @@ std::vector<std::string> parse_input(std::string&& message) {
   return parsed_output;
 }
 
+// Takes in the slave_hostname and the master_hostname
 int main(int argc, char* argv[]) {
   auto hostnames = parse_hostnames(argc, argv);
-  auto hostname = hostnames[0];
+  auto slave_hostname = hostnames[0];
+  auto master_hostname = hostnames[1];
 
   auto const constants = initialize_constants();
   uni::logging::get_log_level() = uni::logging::Level::INFO;
-  LOG(uni::logging::Level::INFO, "Starting client on: " + hostname + ":" + std::to_string(constants.client_port))
+  LOG(uni::logging::Level::INFO, "Starting client. Connecting to slave: '" + slave_hostname + "', and master: '" + master_hostname + "'.")
 
-  boost::asio::io_context io_context;
+  auto io_context = boost::asio::io_context();
   auto work = boost::asio::make_work_guard(io_context);
   std::thread network_thread([&io_context](){
     io_context.run();
   });
 
-  tcp::resolver resolver(io_context);
-  tcp::resolver::results_type endpoints = resolver.resolve(hostname, std::to_string(constants.client_port));
+  auto resolver = tcp::resolver(io_context);
 
-  tcp::socket socket(io_context);
-  boost::asio::connect(socket, endpoints);
-  uni::net::ChannelImpl channel(std::move(socket));
-  channel.add_receive_callback([](std::string serialized_message) {
+  auto slave_endpoints = tcp::resolver::results_type(resolver.resolve(slave_hostname, std::to_string(constants.client_port)));
+  auto socket = tcp::socket(io_context);
+  boost::asio::connect(socket, slave_endpoints);
+  auto slave_channel = uni::net::ChannelImpl(std::move(socket));
+  slave_channel.add_receive_callback([](std::string serialized_message) {
     auto message_wrapper = proto::message::MessageWrapper();
     message_wrapper.ParseFromString(serialized_message);
     LOG(uni::logging::Level::INFO, message_wrapper.DebugString());
     return true;
   });
-  channel.start_listening();
+
+  slave_channel.start_listening();
+
+  auto master_endpoints = tcp::resolver::results_type(resolver.resolve(master_hostname, std::to_string(constants.client_port)));
+  auto socket2 = tcp::socket(io_context);
+  boost::asio::connect(socket2, master_endpoints);
+  auto master_channel = uni::net::ChannelImpl(std::move(socket2));
+  master_channel.add_receive_callback([](std::string serialized_message) {
+    auto message_wrapper = proto::message::MessageWrapper();
+    message_wrapper.ParseFromString(serialized_message);
+    LOG(uni::logging::Level::INFO, message_wrapper.DebugString());
+    return true;
+  });
+
+  master_channel.start_listening();
 
   for (auto request_id = 0;; request_id++) {
     auto message = std::string();
-    std::getline(std::cin, message); // Example input: d, t, k, v
+    std::getline(std::cin, message);
     auto parsed_output = parse_input(std::move(message));
 
-    UNIVERSAL_ASSERT_MESSAGE(parsed_output.size() >= 4,
-      "The input must contain 4 elements, (database_id, table_id, key, value), delimited by a ', '.");
-
-    auto message_wrapper = proto::message::MessageWrapper();
-    auto client_message = new proto::client::ClientMessage();
-    auto request_message = new proto::client::ClientRequest();
-    request_message->set_request_id(std::to_string(request_id));
-    request_message->set_request_type(proto::client::ClientRequest::WRITE);
-    request_message->set_allocated_database_id(uni::utils::pb::string(parsed_output[0]));
-    request_message->set_allocated_table_id(uni::utils::pb::string(parsed_output[1]));
-    request_message->set_allocated_key(uni::utils::pb::string(parsed_output[2]));
-    request_message->set_allocated_value(uni::utils::pb::string(parsed_output[3]));
-    request_message->set_allocated_timestamp(uni::utils::pb::uint64(std::time(nullptr)));
-    client_message->set_allocated_request(request_message);
-    message_wrapper.set_allocated_client_message(client_message);
-    channel.queue_send(message_wrapper.SerializeAsString());
+    if (parsed_output.size() < 1) {
+      LOG(uni::logging::Level::INFO, "Command name missing")
+    }
+    auto const& command = parsed_output[0];
+    if (command == "find") { // Example input: find d t k
+      if (parsed_output.size() < 4) {
+        LOG(uni::logging::Level::INFO, "Missing argument(s): 'database_id' 'table_id' 'key'")
+      }
+      auto message_wrapper = proto::message::MessageWrapper();
+      auto client_message = new proto::client::ClientMessage();
+      auto find_key_request = new proto::client::FindKeyRangeRequest();
+      find_key_request->set_database_id(parsed_output[1]);
+      find_key_request->set_table_id(parsed_output[2]);
+      find_key_request->set_key(parsed_output[3]);
+      client_message->set_allocated_find_key_range_request(find_key_request);
+      message_wrapper.set_allocated_client_message(client_message);
+      master_channel.queue_send(message_wrapper.SerializeAsString());
+    } else if (command == "insert") { // Example input: insert d t k v
+      if (parsed_output.size() < 5) {
+        LOG(uni::logging::Level::INFO, "Missing arguments(s): 'database_id' 'table_id' 'key' 'value'")
+      }
+      auto message_wrapper = proto::message::MessageWrapper();
+      auto client_message = new proto::client::ClientMessage();
+      auto request_message = new proto::client::ClientRequest();
+      request_message->set_request_id(std::to_string(request_id));
+      request_message->set_request_type(proto::client::ClientRequest::WRITE);
+      request_message->set_allocated_database_id(uni::utils::pb::string(parsed_output[1]));
+      request_message->set_allocated_table_id(uni::utils::pb::string(parsed_output[2]));
+      request_message->set_allocated_key(uni::utils::pb::string(parsed_output[3]));
+      request_message->set_allocated_value(uni::utils::pb::string(parsed_output[4]));
+      request_message->set_allocated_timestamp(uni::utils::pb::uint64(std::time(nullptr)));
+      client_message->set_allocated_request(request_message);
+      message_wrapper.set_allocated_client_message(client_message);
+      slave_channel.queue_send(message_wrapper.SerializeAsString());
+    }
   }
 }
